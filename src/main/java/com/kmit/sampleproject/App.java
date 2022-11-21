d47e253b9fcfc1cde0f700107d7b312d88613e79
saisreejaadicherla <118444082+saisreejaadicherla@users.noreply.github.com>
2022-11-21 10:35:24 +0530

    Dmo for the team

---
+++ src/main/java/com/kmit/sampleproject/App.java
@@ -0,0 +1,13 @@
+package com.kmit.sampleproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
